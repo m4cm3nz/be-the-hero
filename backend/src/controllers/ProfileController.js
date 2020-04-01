2a2a429568f44faa5231359271f3f959d9249a7d
Rafael Mac Menz <rmcmenz@gmail.com>
2020-04-01 00:28:39 -0300

    first commit

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,14 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(request, response) {
+        const omg_id = request.headers.authorization;
+
+        const incidents = await connection('incidents')
+            .where('ong_id', omg_id)
+            .select('*');
+
+        return response.json(incidents);
+    }
+}
